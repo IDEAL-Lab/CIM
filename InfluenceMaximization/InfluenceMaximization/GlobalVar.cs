7fc2ccea13db9b29be6599a35a4de7e5462a3c5f
punkcpp <punkcpp@gmail.com>
2016-08-31 11:41:06 -0700

    Update GlobalVar.cs

--- InfluenceMaximization/InfluenceMaximization/GlobalVar.cs
+++ InfluenceMaximization/InfluenceMaximization/GlobalVar.cs
@@ -11,11 +11,12 @@ namespace InfluenceMaximization
 		public static int MC = 80000;
 		public static int mH = 5000000;
 		public static int batch_num = 100; // number of batches of iterations in Coordinate Descent Algorithm
-        public static double Alpha = 1.0;
+        	public static double Alpha = 1.0;
+        	public static double b = 0.05;
 
-        // St = 1 and End = 5 mean we set B = 10,20,30,40,50 and run CIM algorithms respectively
-        public static int St = 1;
-        public static int End = 5;
+        	// St = 1 and End = 5 mean we set B = 10,20,30,40,50 and run CIM algorithms respectively
+        	public static int St = 1;
+        	public static int End = 5;
 	}
 }
 
